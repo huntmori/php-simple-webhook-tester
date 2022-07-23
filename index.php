d5a15d5a91a65bf0a002406784d73e97d3b8022a
Nhyun Kim <nhyun5050@gmail.com>
2022-07-23 10:03:21 +0000

    컨트롤러 클래스 구조로 변경

--- index.php
+++ index.php
@@ -5,52 +5,62 @@
 
     use Monolog\Handler\StreamHandler;
     use Monolog\Logger;
-    $base_url = "/api/v1/chat";
-    $pun_webhook = "/api/v1/pun/room";
 
-    $loggingMethod = function($requestObject, $responseObject) {
-        $logger = new Logger('logger');
-        $logger->pushHandler(new StreamHandler('./logs/'.date('Ymd').'.log'));
-        $logger->info("[".$requestObject->url."]");
-        $logger->info("request-query=========================================================================");
-        $logger->info(json_encode($requestObject->query));
-        $logger->info("request-body==========================================================================");
-        $logger->info(json_encode($requestObject->data));
-        $logger->info("response==============================================================================");
-        $logger->info(json_encode($responseObject));
-        $logger->info("======================================================================================");
-    };
-    
-    $schemeMethod = function($requestObject) {
-        $fileName = str_replace('/', '.', $requestObject->url);
-        $schemeFileExsist = file_exists("./scheme/$fileName.json");
-        
-        if (!$schemeFileExsist) {
-            $output = fopen("./scheme/$fileName.json", "a");
-            fwrite($output, json_encode($requestObject->data));
-            fclose($output);
-        }
-    };
+    class Controller {
+		function loggingMethod($requestObject, $responseObject) {
+			if (is_dir('./logs') === false ) {
+				mkdir('./logs', 0755, false);
+			}
 
-    $chatWebhookCallBack = function($loggingMethod, $schemeMethod) {
-        $request = Flight::request();
-        $base_response = array("ResultCode"=>0, "DebugMessage"=>"OK");
-        Flight::json($base_response);
+			$logger = new Logger('logger');
+			$logger->pushHandler(new StreamHandler('./logs/'.date('Ymd').'.log'));
+			$logger->info("[".$requestObject->url."]");
+			$logger->info("request-query=========================================================================");
+			$logger->info(json_encode($requestObject->query));
+			$logger->info("request-body==========================================================================");
+			$logger->info(json_encode($requestObject->data));
+			$logger->info("response==============================================================================");
+			$logger->info(json_encode($responseObject));
+			$logger->info("======================================================================================");
+		}
 
-        $loggingMethod($request, $base_response);
-        $schemeMethod($request);
-    };
+		 function schemeMethod($requestObject) {
+			if (is_dir('./scheme') === false ) {
+				mkdir('./scheme', 0755, false);
+			}
 
-    $punWebhookCallBack = function($loggingMethod, $schemeMethod) {
-        $request = Flight::request();      
-        $base_response = array("ResultCode"=>0, "ErrorCode"=>0);        
-        Flight::json($base_response);
-        
-        $loggingMethod($request, $base_response);
-        $schemeMethod($request);
-    };
-    Flight::route("POST /api/v1/chat/*",        $chatWebhookCallBack($loggingMethod, $schemeMethod));
-    Flight::route("POST /api/v1/pun/room/*",    $punWebhookCallBack($loggingMethod, $schemeMethod));
+			$fileName = str_replace('/', '.', $requestObject->url);
+			$schemeFileExsist = file_exists("./scheme/$fileName.json");
+
+			if (!$schemeFileExsist) {
+				$output = fopen("./scheme/$fileName.json", "a");
+				fwrite($output, json_encode($requestObject->data));
+				fclose($output);
+			}
+		}
+
+		function chatWebhookCallBack() {
+			$request = Flight::request();
+			$base_response = array("ResultCode"=>0, "DebugMessage"=>"OK");
+			Flight::json($base_response);
+
+			$this->loggingMethod($request, $base_response);
+			$this->schemeMethod($request);
+		}
+
+		function punWebhookCallBack() {
+			$request = Flight::request();      
+			$base_response = array("ResultCode"=>0, "ErrorCode"=>0);        
+			Flight::json($base_response);
+
+			$this->loggingMethod($request, $base_response);
+			$this->schemeMethod($request);
+		}
+	}
+
+    $controller = new Controller();
+    Flight::route("POST /api/v1/chat/*",        array($controller, 'chatWebhookCallBack'));
+    Flight::route("POST /api/v1/pun/room/*",    array($controller, 'punWebhookCallBack'));
 
     Flight::route('*', function(){
         Flight::json(array("bye"=>"guys"));
